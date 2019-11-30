7fd122ac9190db8d2481e8cd55d69c73432ae5bf
obr.note <k33mdukb9n@yahoo.co.jp>
2019-11-30 12:11:23 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'users#new'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
 end
